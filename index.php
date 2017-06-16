3398551dc7d967da5d68f76f382d975da4a5ec35
Arun Anson Arouje <arun@helloinfinity.com>
2017-06-16 14:55:34 +0530

    added Login/Registration API and function scratchpad

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+	//
+	//	Question2Answer API
+	//	Author : Arun Anson
+	//	Copyright (c) 2017 Hello Infinity Business Solutions Pvt. Ltd.
+	//	15th June 2017
+	//
+	
+	//Q2A Location on server
+	define("Q2ALOCATION", "../q2a");
+
+	include 'login.php';
+
+	//Get JSON Request
+	$json_request = json_decode(file_get_contents('php://input'), true);
+
+	$interactionCode	= $json_request['requestHeader']['interactionCode'];
+	$serviceId	= $json_request['requestHeader']['serviceId'];
+
+	switch ($interactionCode) {
+
+		case 'LOGIN':
+			{
+				login($json_request);
+			}	
+			break;
+
+		default:
+			{
+				echo '{"responseHeader"	:	{
+							"serviceId": "'.$serviceId.'",
+							"status": "405",
+							"message": "Method Not Allowed"
+						}
+					}';
+			}
+			break;
+	}
+?>
