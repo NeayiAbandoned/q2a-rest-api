c0a3d21a0f63d265d42d308006a43429522ac33b
Arun Anson Arouje <arun@helloinfinity.com>
2017-07-13 16:01:36 +0530

    Updated GET QUESTION DETAIL with answered to yes if the question was answered earlier. also implementd userid inside question to facilitate images with question

--- get-question-detail.php
+++ get-question-detail.php
@@ -9,7 +9,7 @@
 	// 	Gets all of the questions and its details in order they are posted.
 
 	// 	Sample Input
-	// { "requestHeader": { "serviceId":"111", "interactionCode":"GETQUESTIONDETAIL"}, "requestBody" : { "questionid" : "1" }}
+	// { "requestHeader": { "serviceId":"111", "interactionCode":"GETQUESTIONDETAIL"}, "requestBody" : { "questionid" : "1", "user_id" : "user" }}
 
 	// 	Sample Output
 	//	{"responseHeader":{"serviceId":"111","status":"200"},"responseBody":{"question":[{"title":"update post title","acount":"7","views":"1","tags":"tag1 update, tag2 update1","content":"update content","netvotes":"1","updated":null,"created":"1497455725"}],"answers":[{"title":null,"postid":"20","acount":"0","views":"0","tags":null,"content":"?one ?one ?one ?one ?one ?one ?one ?one","netvotes":"0","updated":null,"created":"1498057104"},{"title":null,"postid":"19","acount":"0","views":"0","tags":null,"content":"here goes an another another answer","netvotes":"0","updated":null,"created":"1498056816"},{"title":null,"postid":"18","acount":"0","views":"0","tags":null,"content":"here goes an another answer","netvotes":"0","updated":null,"created":"1498056770"},{"title":null,"postid":"17","acount":"0","views":"0","tags":null,"content":"here goes an answer","netvotes":"0","updated":null,"created":"1498056746"},{"title":null,"postid":"16","acount":"0","views":"0","tags":null,"content":"answer test content","netvotes":"0","updated":null,"created":"1498056517"},{"title":null,"postid":"9","acount":"0","views":"0","tags":null,"content":"Answer2 on the test question","netvotes":"0","updated":null,"created":"1497697943"},{"title":null,"postid":"2","acount":"0","views":"0","tags":null,"content":"answer test test","netvotes":"1","updated":null,"created":"1497455825"}]}}
@@ -20,8 +20,12 @@
 
 	$serviceId	= $json_request['requestHeader']['serviceId'];
 	$questionid = $json_request['requestBody']['questionid'];
+	$current_user_id = $json_request['requestBody']['user_id'];
+	
+	//Set answered flag to no
+	$answered = 'no';
 
-		$sql_get_question = "SELECT title, acount, views, tags, content, netvotes, UNIX_TIMESTAMP(updated) as updated, UNIX_TIMESTAMP(created) as created FROM ".TABLEPREFIX."posts WHERE type='Q' and postid='".$questionid."' ORDER BY created DESC LIMIT 20;";
+		$sql_get_question = "SELECT title, acount, userid, views, tags, content, netvotes, UNIX_TIMESTAMP(updated) as updated, UNIX_TIMESTAMP(created) as created FROM ".TABLEPREFIX."posts WHERE type='Q' and postid='".$questionid."' ORDER BY created DESC LIMIT 20;";
 		$result_get_question = $conn->query($sql_get_question);
 
 		while($row_get_question = $result_get_question->fetch_assoc()) {
@@ -34,7 +38,14 @@
 		$result_get_question_detail = $conn->query($sql_get_question_detail);
 		
 		while($row_get_question_detail = $result_get_question_detail->fetch_assoc()) {
+            
             $data_get_question_detail[] = $row_get_question_detail;
+
+            //If the user has answered the question set answered to yes.
+            if ($row_get_question_detail['userid'] == $current_user_id && $row_get_question_detail['userid'] != '') {
+
+            	$answered = 'yes';
+            }
         }
 
         $num_rows = mysqli_num_rows($result_get_question_detail);
@@ -45,6 +56,7 @@
 			$res['responseHeader']['serviceId'] = $serviceId;
 			$res['responseHeader']['status'] = "200";
 			$res['responseBody']['question'] = $data_get_question;
+			$res['responseBody']['question'][0]['answered'] = $answered;
 			$res['responseBody']['answers'] = $data_get_question_detail;
 		}else{
 
